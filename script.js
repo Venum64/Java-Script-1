6151b0139725882879247e45f77f80a3291565e9
venum <umarhon167@gm.com>
2025-09-04 21:22:50 +0500

    js changed

--- script.js
+++ script.js
@@ -10,23 +10,23 @@ console.log("Мне " + age + " лет");
 
 // examples start
 let answer1 = +prompt("Решите пример 6 + 3")
-console.log("Пример 1: " + "6 + 3 = 9 " + " Ваш ответ: " + answer1);
+console.log("Пример 1: " + "6 + 3 = " + (6 + 3) + " Ваш ответ: " + answer1);
 let answer2 = +prompt("Решите пример 20 - 5")
-console.log("Пример 2: " + "20 - 5 = 15 " + " Ваш ответ: " + answer2);
+console.log("Пример 2: " + "20 - 5 = " + (20 - 5) + " Ваш ответ: " + answer2);
 let answer3 = +prompt("Решите пример 15 * 2")
-console.log("Пример 3: " + "15 * 2 = 30 " + " Ваш ответ: " + answer3);
+console.log("Пример 3: " + "15 * 2 = " + (15 * 2) + " Ваш ответ: " + answer3);
 let answer4 = +prompt("Решите пример 8 / 4")
-console.log("Пример 4: " + "8 / 4 = 2 " + " Ваш ответ: " + answer4);
+console.log("Пример 4: " + "8 / 4 = " + (8 / 4) + " Ваш ответ: " + answer4);
 let answer5 = +prompt("Решите пример 10 % 3")
-console.log("Пример 5: " + "10 % 3 = 1 " + " Ваш ответ: " + answer5);
+console.log("Пример 5: " + "10 % 3 = " + (10 % 3) + " Ваш ответ: " + answer5);
 confirm("Зайдите в консоль для просмотра")
 // examples end
 
 // numbers start
 
-var X, Y, Z, res;
-X =  +prompt("Введите первое число")
-Y = +prompt("Введите второе число")
-Z = +prompt("Введите третье число")
-res = confirm("Ваше среднее арифметическое значение чисел цифра " + (X + Y + Z) / 3)
+var X = +prompt("Введите первое число")
+var Y = +prompt("Введите второе число")
+var Z = +prompt("Введите третье число")
+var res = (X + Y + Z) / 3;
+alert("Ваше среднее арифметическое значение чисел цифра " + res)
 // numbers end
